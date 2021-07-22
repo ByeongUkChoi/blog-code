2dce159ffdf0672a58395a338af13dc453ea55a3
ByeongUk Choi <cbw0916@gmail.com>
2021-07-22 12:22:01 +0900

    Add datasource initialization options

---
+++ querydsl-group-by-test/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO members (name, age) VALUES ('AAA', 10);
+INSERT INTO members (name, age) VALUES ('AAA', 10);
+INSERT INTO members (name, age) VALUES ('AAA', 12);
+INSERT INTO members (name, age) VALUES ('AAA', 15);
+INSERT INTO members (name, age) VALUES ('BBB', 10);
+INSERT INTO members (name, age) VALUES ('BBB', 12);
+INSERT INTO members (name, age) VALUES ('CCC', 15);
